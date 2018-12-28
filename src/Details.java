3467610131a9f4214449f923207c2b8bf8d21958
timkit <kti80@mail.ru>
2018-12-28 21:06:48 +0600

    New files Learn

---
+++ src/Details.java
@@ -0,0 +1,16 @@
+
+public class Details {
+	// Member variables
+	private String n;
+	private String s;
+	private int c;
+	private double m;
+	// Constructor
+	public Details(String name, String spec,int count, double mass ) {
+		this.n=name;
+		this.s=spec;
+		this.c=count;
+		this.m=mass;		
+	}
+
+}
