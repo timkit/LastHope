e311613cbced5dac2db825c7253a9f743a605ad2
timkit <kti80@mail.ru>
2019-01-02 13:28:11 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 
 public class Main {
 
+	public static int flip(int a, int b) {
+		int x;
 
+		System.out.println("A=" + String.format("%32s", Integer.toBinaryString(a)).replace(' ', '0'));
+		System.out.println("B=" + b+"\n");
+		
+		System.out.println("S=" + String.format("%32s", Integer.toBinaryString(a << b)).replace(' ', '0'));
+		System.out.println("B=" + String.format("%32s", Integer.toBinaryString(b)).replace(' ', '0')+"\n");
+		x =a ^ (1 << (b-1));
+		System.out.println("X=" + String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0')+"\n");
+		return x;
+	}
 
 	public static void main(String[] args) {
 
+		System.out.println("Decimal:" + (flip(2, 1)));
 
 	}
-
 }
